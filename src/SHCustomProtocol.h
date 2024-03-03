b7dc3eb4189a14decbded7a4298ecbd0deec3985
Ecor <3725293+ecoreng@users.noreply.github.com>
2024-03-03 15:13:35 -0800

    Update to add config for 8048S043C

--- src/SHCustomProtocol.h
+++ src/SHCustomProtocol.h
@@ -7,6 +7,7 @@
 
 #define TFT_BL 2 // backlight pin
 
+// 4827S043 - 480x270, no touch
 Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
     40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
     45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
@@ -15,6 +16,18 @@ Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
     0 /* hsync_polarity */, 1 /* hsync_front_porch */, 1 /* hsync_pulse_width */, 43 /* hsync_back_porch */,
     0 /* vsync_polarity */, 3 /* vsync_front_porch */, 1 /* vsync_pulse_width */, 12 /* vsync_back_porch */,
     1 /* pclk_active_neg */, 10000000 /* prefer_speed */);
+
+// https://github.com/eCrowneEng/ESP-SimHub-ESP32S3-SCREEN/issues/1
+// 8048S043 - 800x480, capacitive touch
+//Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
+//    40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
+//    45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
+//    5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
+//    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */,
+//    0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 16 /* hsync_back_porch */,
+//    0 /* vsync_polarity */, 4 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 4 /* vsync_back_porch */,
+//    1 /* pclk_active_neg */, 16000000 /* prefer_speed */);
+
 
 Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
     PIXEL_WIDTH /* width */, PIXEL_HEIGHT /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */);
